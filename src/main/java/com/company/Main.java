d2e903031284fd227982622391197c00024295f6
dkosmakov <dkosmakov@playtika.com>
2020-02-21 02:00:48 +0200

    Solve C with dyn prog.

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -11,31 +11,143 @@ import static java.util.Arrays.stream;
 
 public class Main {
 
-    public static final String FILE_NAME = "b_read_on";
+    public static final String FILE_NAME = "c_incunabula";
+    public static int[] bookScores;
 
     public static void main(String[] args) {
-
         InputStream inputStream = Main.class.getClassLoader().getResourceAsStream(FILE_NAME + ".txt");
         Scanner sc = new Scanner(inputStream);
         int books = sc.nextInt();
         int libCount = sc.nextInt();
         int days = sc.nextInt();
 
-        int[] bookScores = new int[books];
+        bookScores = new int[books];
         for (int i = 0; i < books; i++) {
             bookScores[i] = sc.nextInt();
         }
 
-        Library[] libraries = new Library[libCount];
+        SetLibrary[] libraries = new SetLibrary[libCount];
         for (int i = 0; i < libCount; i++) {
-            libraries[i] = readLibrary(sc, bookScores, i);
+            libraries[i] = readLibraryToSet(sc, bookScores, i);
         }
 
         Input input = new Input(libraries, days, bookScores);
-        solveB(input);
+        solveCDynProg(input);
+    }
+
+    public static void solveCDynProg(Input input) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int totalLibs = 0;
+
+        int[][] dp = new int[input.setLibraries.length + 1][input.days + 1];
+        for (int i = 0; i < input.days; i++) {
+            dp[0][i] = 0;
+        }
+
+        for (int i = 1; i <= input.setLibraries.length; i++) {
+            for (int j = 0; j <= input.days; j++) {
+                SetLibrary setLibrary = input.setLibraries[i - 1];
+                if (setLibrary.signUpDays + 1 < j) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - setLibrary.signUpDays] + setLibrary.libraryScore);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        int res = dp[input.setLibraries.length][input.days];
+
+        int elem = input.setLibraries.length;
+        int days = input.days;
+        int total = 0;
+        while (elem > 0 && res > 0) {
+            if (dp[elem - 1][days] == res) {
+                elem--;
+            } else {
+                totalLibs++;
+                SetLibrary lib = input.setLibraries[elem - 1];
+                stringBuilder.append(lib.id)
+                        .append(" ")
+                        .append(lib.booksCount)
+                        .append("\n");
+                stringBuilder.append(
+                        lib.books.stream()
+                                .map(String::valueOf)
+                                .collect(Collectors.joining(" ")
+                                )
+                );
+                stringBuilder.append("\n");
+                elem--;
+                res -= lib.libraryScore;
+                days -= lib.signUpDays;
+                total += lib.libraryScore;
+            }
+        }
+
+        stringBuilder = new StringBuilder().append(totalLibs).append("\n").append(stringBuilder);
+
+        try {
+            Files.write(Paths.get(FILE_NAME + ".out"), stringBuilder.toString().getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(Arrays.toString(dp[input.setLibraries.length]));
+        System.out.println(total);
     }
 
     public static void solveC(Input input) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int totalLibs = 0;
+        int daysRemaining = input.days;
+        Arrays.sort(input.setLibraries, (o1, o2) -> {
+            if (o1.signUpDays < o2.signUpDays) {
+                return -1;
+            } else if (o1.signUpDays > o2.signUpDays) {
+                return 1;
+            } else {
+                return Integer.compare(o2.calcScore(), o1.calcScore());
+            }
+        });
+        for (int i = 0; i < input.setLibraries.length; i++) {
+            SetLibrary lib = input.setLibraries[i];
+            if (lib.signUpDays + 1 <= daysRemaining) {
+                totalLibs++;
+                stringBuilder.append(lib.id)
+                        .append(" ")
+                        .append(lib.booksCount)
+                        .append("\n");
+                stringBuilder.append(
+                        lib.books.stream()
+                                .map(String::valueOf)
+                                .collect(Collectors.joining(" ")
+                                )
+                );
+                stringBuilder.append("\n");
+                for (int j = i + 1; j < input.setLibraries.length; j++) {
+                    SetLibrary libInternal = input.setLibraries[j];
+                    libInternal.deleteBooks(lib.books);
+                }
+                daysRemaining -= lib.signUpDays;
+                Arrays.sort(input.setLibraries, i, input.setLibraries.length, (o1, o2) -> {
+                    if (o1.signUpDays < o2.signUpDays) {
+                        return -1;
+                    } else if (o1.signUpDays > o2.signUpDays) {
+                        return 1;
+                    } else {
+                        return Integer.compare(o2.calcScore(), o1.calcScore());
+                    }
+                });
+            }
+
+        }
+
+        stringBuilder = new StringBuilder().append(totalLibs).append("\n").append(stringBuilder);
+
+        try {
+            Files.write(Paths.get(FILE_NAME + ".out"), stringBuilder.toString().getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -133,13 +245,37 @@ public class Main {
         return library;
     }
 
+    public static SetLibrary readLibraryToSet(Scanner sc, int[] bookScores, int id) {
+        int booksCount = sc.nextInt();
+        int signUpProcess = sc.nextInt();
+        int booksPerDay = sc.nextInt();
+        int libraryScore = 0;
+
+        SetLibrary library = new SetLibrary(id, booksCount, signUpProcess, booksPerDay);
+        for (int i = 0; i < booksCount; i++) {
+            int bookId = sc.nextInt();
+            library.books.add(bookId);
+            libraryScore += bookScores[bookId];
+        }
+        library.libraryScore = libraryScore;
+
+        return library;
+    }
+
     public static class Input {
         public Library[] libraries;
+        public SetLibrary[] setLibraries;
         public int days;
         public int[] bookScores;
 
         public Input(Library[] libraries, int days, int[] bookScores) {
             this.libraries = libraries;
+            this.days = days;
+            this.bookScores = bookScores;
+        }
+
+        public Input(SetLibrary[] libraries, int days, int[] bookScores) {
+            this.setLibraries = libraries;
             this.days = days;
             this.bookScores = bookScores;
         }
@@ -159,6 +295,34 @@ public class Main {
             this.signUpDays = signUpDays;
             this.booksPerDay = booksPerDay;
             books = new int[booksCount];
+        }
+    }
+
+    public static class SetLibrary {
+        public int id;
+        public int booksCount;
+        public int signUpDays;
+        public int booksPerDay;
+        public Set<Integer> books;
+        public int libraryScore = 0;
+
+        public SetLibrary(int id, int booksCount, int signUpDays, int booksPerDay) {
+            this.id = id;
+            this.booksCount = booksCount;
+            this.signUpDays = signUpDays;
+            this.booksPerDay = booksPerDay;
+            books = new HashSet<>();
+        }
+
+        public void deleteBooks(Set<Integer> bookIds) {
+            books.removeAll(bookIds);
+            this.booksCount = books.size();
+        }
+
+        public int calcScore() {
+            libraryScore = 0;
+            books.forEach(i -> libraryScore += bookScores[i]);
+            return libraryScore;
         }
     }
 
